8e10838b5a01d670920bc53aa9b0210152d5601e
IsalySultan <IsalySultan@users.noreply.github.com>
2014-11-25 01:30:53 +0300

    Create CommandSize.java

---
+++ src/ru/fizteh/fivt/students/isalysultan/JUnit/CommandSize.java
@@ -0,0 +1,9 @@
+package ru.fizteh.fivt.students.isalysultan.JUnit;
+
+import java.io.IOException;
+
+public class CommandSize {
+    public static int size(MyTable table) throws IOException {
+        return table.get();
+    }
+}
