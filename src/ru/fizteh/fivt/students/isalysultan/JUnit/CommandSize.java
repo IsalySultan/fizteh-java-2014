a6f7974b46d9a863c95f2acd0a0d598c9d602493
IsalySultan <IsalySultan@users.noreply.github.com>
2014-12-22 17:32:39 +0300

    Delete CommandSize.java

--- src/ru/fizteh/fivt/students/isalysultan/JUnit/CommandSize.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.isalysultan.JUnit;
-
-import java.io.IOException;
-
-public class CommandSize {
-    public static int size(MyTable table) throws IOException {
-        return table.get();
-    }
-}
