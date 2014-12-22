d1ab141f39984f71d7b18c77c68c298be35e7c4d
IsalySultan <IsalySultan@users.noreply.github.com>
2014-12-22 17:31:05 +0300

    Delete CommandCommit.java

--- src/ru/fizteh/fivt/students/isalysultan/JUnit/CommandCommit.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.isalysultan.JUnit;
-
-public class CommandCommit {
-    public static int commit(MyTable table) {
-        int result = table.getCurrentChanges();
-        table.saveTable();
-        return result;
-    }
-}
