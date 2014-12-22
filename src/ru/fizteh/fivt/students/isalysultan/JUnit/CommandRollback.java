198fe426410661001fcb6072e4c713bebd83551e
IsalySultan <IsalySultan@users.noreply.github.com>
2014-12-22 17:32:24 +0300

    Delete CommandRollback.java

--- src/ru/fizteh/fivt/students/isalysultan/JUnit/CommandRollback.java
+++
@@ -1,9 +0,0 @@
-package ru.fizteh.fivt.students.isalysultan.JUnit;
-
-public class CommandRollback {
-    public static int rollback(MyTable table) {
-        int result = table.getCommitChanges();
-        table.filesRollback();
-        return result;
-    }
-}
